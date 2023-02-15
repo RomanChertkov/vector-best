54900b719ce66067c1999a837caeb6115ba0d36f
Roman Chertkov <r.chertkov@mail.ru>
2023-02-15 11:30:27 +0700

    Added app pages and layout

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import MainLayout from '../layouts/MainLayout'
+import HomePage from '../pages/HomePage'
+import PokemonPage from '../pages/PokemonPage'
+import NoMatch from '../pages/NoMatch'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="pokemon" element={<PokemonPage />} />
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
